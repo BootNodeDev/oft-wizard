22fb2fccc555d4fc5058c38acb39a87684fb7eae
p4p4_l3o <lpuglisi@epe.santafe.gov.ar>
2025-04-01 10:55:19 -0300

    Adding .vscode to gitignore

--- core/src/client.rs
+++ core/src/client.rs
@@ -4,6 +4,7 @@ use std::path::Path;
 
 /// Load a wallet from a keystore file.
 /// `cast wallet new .`
+// comment example
 pub async fn wallet_from_keystore(path: &str, password: &str) -> Result<LocalWallet> {
     let path = Path::new(path);
     let wallet = Wallet::decrypt_keystore(&path, password)?;
